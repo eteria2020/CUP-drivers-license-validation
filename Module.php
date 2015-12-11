d4c5374f3a8069858cfca67c7fb9134d986cc653
Marco Perone <m.perone@mvlabs.it>
2015-12-11 16:48:13 +0100

    add events to avoid forking problems with database connection

--- Module.php
+++ Module.php
@@ -12,12 +12,27 @@ class Module
         return include __DIR__ . '/config/module.config.php';
     }
 
-    public function onBootstrap()
+    public function onBootstrap(MvcEvent $e)
     {
         $retryPlugin = new Retry();
 
         Resque_Event::listen('beforePerform', [$retryPlugin, 'beforePerform']);
         Resque_Event::listen('afterPerform', [$retryPlugin, 'afterPerform']);
         Resque_Event::listen('onFailure', [$retryPlugin, 'onFailure']);
+
+        $application = $e->getApplication();
+
+        Resque_Event::listen('beforeFork', function () use ($application) {
+            $serviceManager = $application->getServiceManager();
+            $entityManager = $serviceManager->get('doctrine.entitymanager.orm_default');
+
+            $entityManager->getConnection()->close();
+        });
+        Resque_Event::listen('beforePerform', function () use ($application) {
+            $serviceManager = $application->getServiceManager();
+            $entityManager = $serviceManager->get('doctrine.entitymanager.orm_default');
+
+            $entityManager->getConnection()->connect();
+        });
     }
 }
